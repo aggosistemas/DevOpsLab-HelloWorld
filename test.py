1b17e326b3ad0e8b3cb7f270b6ce8949863cd5b0
antonio oliveira <aggosistemas@gmail.com>
2023-02-20 09:49:04 -0500

    alterando sozinho os arquivos

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline de estudo Antonio Oliveira aggodevops")
+        self.assertRegex(result.data.decode(), "Pipeline de estudo")
 
 
 if __name__ == "__main__":
