11e43f4ad5c1c4b116335f77e39b5f89fd93cb25
antonio oliveira <aggosistemas@gmail.com>
2023-02-20 09:04:44 -0500

    Alterando o cabecalho

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabecalho da Pagina.")
+        self.assertRegex(result.data.decode(), "Pipeline de estudo Antonio Oliveira aggodevops.")
 
 
 if __name__ == "__main__":
